06c0945cb8c4d015e07325d322686dcf6280aba5
Jaroslaw Glod <emmart@poczta.onet.pl>
2024-04-09 11:11:22 +0100

    Faq added but very simple way, must be improved

--- scripts/script.js
+++ scripts/script.js
@@ -35,3 +35,26 @@ window.onclick = function(event) {
     }
   }
 
+  function faq1(){
+    document.getElementById("answer").innerHTML="We have no idea where to buy tickets. We only organise concerts and events. They don't pay us for ticket distribution.";
+  }
+  function faq2(){
+    document.getElementById("answer").innerHTML="It is your problem. Do you thin we are going to call you and remind about an event, or maybe send a car for you? C'mon!";
+  }
+
+  function faq3(){
+    document.getElementById("answer").innerHTML="In general, you may wear many different colors, but yes, there are some restrictions. No rgb(75,12,68) trousers.";
+  }
+
+  function faq4(){
+    document.getElementById("answer").innerHTML="Of course you can bring your alcohol and drugs. If you want to buy, there are also many dealers arond.";
+  }
+
+  function faq5(){
+    document.getElementById("answer").innerHTML=" You can bring how many children you want. We are children friendly. There is free alcohol and weed for minors.";
+  }
+
+  function faq6(){
+    document.getElementById("answer").innerHTML="My favourite color is rgb(75,12,68)";
+  }
+
