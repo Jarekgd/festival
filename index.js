2460a9577f0e76fa35445d4f92814d77133b4d88
Jaroslaw Glod <emmart@poczta.onet.pl>
2024-04-14 16:43:27 +0100

    Validated by W3c corrected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const cookie = require("cookie-parser");
+const PORT = process.env.PORT || 5000;
 
 app.set("View engine", "ejs");
 // Placement of static files:
